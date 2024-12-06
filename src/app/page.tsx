50229dcf51246d74849737f126548bb19f9132cd
kimsohyeon <aotoyae@gmail.com>
2024-12-06 16:11:07 +0900

    feat: 리드 목록 페이지 생성 및, 리드 기본 정보 표시

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import Menu from "@/components/Menu";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,6 +10,11 @@ export default function Home() {
         <Menu />
         <section>
           <h1>peak</h1>
+          <Link href="/lead">
+            <button className="rounded border border-solid bg-gray-300 p-3">
+              리드 아이템 페이지로
+            </button>
+          </Link>
         </section>
       </main>
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between bg-black p-4 text-white">
+    <header className="flex h-[10vh] items-center justify-between bg-black px-4 text-white">
       <Link href="/">
         <h1 className="text-2xl font-bold">peak</h1>
       </Link>
