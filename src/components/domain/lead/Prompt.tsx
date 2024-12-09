a5d95ac3a8a2af61cd87da0f27afb0244de5541b
kimsohyeon <aotoyae@gmail.com>
2024-12-10 01:11:20 +0900

    feat: 프롬프트 화면 구성

--- src/components/domain/lead/Prompt.tsx
+++ src/components/domain/lead/Prompt.tsx
@@ -1,5 +1,13 @@
 const Prompt = () => {
-  return <div>Prompt</div>;
+  return (
+    <main className="flex flex-col items-end justify-start gap-4 p-4">
+      <button className="rounded bg-neutral-300 px-10 py-2">소스 추가</button>
+      <section className="flex h-32 w-1/5 flex-col items-center justify-between self-start rounded border border-solid border-neutral-300 bg-white p-4 drop-shadow">
+        <h4 className="text-xl font-bold">인공지능 아바타 판매</h4>
+        <p className="self-end">2024-00-00</p>
+      </section>
+    </main>
+  );
 };
 
 export default Prompt;
