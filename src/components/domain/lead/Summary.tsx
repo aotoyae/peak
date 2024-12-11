a0c9f2cc3c8d1b3060ff97e59b7d4e84a8bb3294
kimsohyeon <aotoyae@gmail.com>
2024-12-11 17:40:37 +0900

    feat: GPCT C&I 화면 구성

--- src/components/domain/lead/Summary.tsx
+++ src/components/domain/lead/Summary.tsx
@@ -14,17 +14,13 @@ const Summary = () => {
         <p className="h-full w-full rounded border border-solid border-neutral-300 bg-neutral-100 p-2"></p>
         <button className="rounded bg-neutral-300 px-10 py-2">저장하기</button>
       </section>
-      <section className="p-4">
-        <form className="flex h-[480px] flex-col items-end justify-center gap-4">
-          <textarea
-            className="h-full w-full rounded border border-solid border-neutral-300 bg-neutral-100 p-2"
-            placeholder="메모 작성"
-          ></textarea>
-          <button className="rounded bg-neutral-300 px-10 py-2">
-            저장하기
-          </button>
-        </form>
-      </section>
+      <form className="flex h-[480px] flex-col items-end justify-center gap-4 p-4">
+        <textarea
+          className="h-full w-full rounded border border-solid border-neutral-300 bg-neutral-100 p-2"
+          placeholder="메모 작성"
+        ></textarea>
+        <button className="rounded bg-neutral-300 px-10 py-2">저장하기</button>
+      </form>
     </main>
   );
 };
