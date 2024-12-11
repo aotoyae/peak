158d30e94b21755caef1ba6d80fb75d0f578d1d1
kimsohyeon <aotoyae@gmail.com>
2024-12-11 17:48:51 +0900

    feat: 메모 화면 구성

--- src/components/domain/lead/Details.tsx
+++ src/components/domain/lead/Details.tsx
@@ -59,7 +59,13 @@ const Details = () => {
           저장하기
         </button>
       </form>
-      <section></section>
+      <form className="flex flex-col items-end justify-between gap-4 p-4">
+        <textarea
+          className="h-32 w-full rounded border border-solid border-neutral-300 bg-neutral-100 p-2"
+          placeholder="메모 작성"
+        ></textarea>
+        <button className="rounded bg-neutral-300 px-10 py-2">저장하기</button>
+      </form>
     </main>
   );
 };
--- src/components/domain/lead/Summary.tsx
+++ src/components/domain/lead/Summary.tsx
@@ -14,7 +14,7 @@ const Summary = () => {
         <p className="h-full w-full rounded border border-solid border-neutral-300 bg-neutral-100 p-2"></p>
         <button className="rounded bg-neutral-300 px-10 py-2">저장하기</button>
       </section>
-      <form className="flex h-[480px] flex-col items-end justify-center gap-4 p-4">
+      <form className="flex h-[580px] flex-col items-end justify-center gap-4 p-4">
         <textarea
           className="h-full w-full rounded border border-solid border-neutral-300 bg-neutral-100 p-2"
           placeholder="메모 작성"
