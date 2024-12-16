3a1a4fe8b8a6cbdd432847933b66908742dcd799
kimsohyeon <aotoyae@gmail.com>
2024-12-16 17:28:54 +0900

    refactor: 모달 컨텐츠 컴포넌트 분리

--- src/components/domain/lead/SourceUploadModal.tsx
+++ src/components/domain/lead/SourceUploadModal.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useState } from "react";
+import ModalHeader from "./ModalHeader";
+import ModalContents from "./ModalContents";
 
 const SourceUploadModal = ({
   setMadalOpen,
@@ -35,24 +37,7 @@ const SourceUploadModal = ({
   return (
     <section className="fixed inset-0 flex items-center justify-center bg-neutral-950 bg-opacity-50">
       <div className="flex w-[50vw] flex-col gap-6 rounded-2xl bg-neutral-800 p-6">
-        <div className="flex justify-between">
-          <div className="flex flex-col justify-between gap-2">
-            <h2 className="text-3xl font-black text-neutral-300">소스 추가</h2>
-            <p className="text-lg leading-normal text-neutral-400">
-              소스를 추가하면 Peak 가 가장 중요한 정보에 따라 응답을 제공합니다.
-              <br />
-              (예: 주요 기술, 마케팅 계획, 연구 노트 등)
-            </p>
-          </div>
-          <button
-            onClick={() => {
-              setMadalOpen(false);
-            }}
-            className="h-14 w-14 rounded-full bg-neutral-500 p-4 text-2xl font-black leading-7 text-neutral-300"
-          >
-            ✕
-          </button>
-        </div>
+        <ModalHeader setMadalOpen={setMadalOpen} />
         <div className="">
           <input
             type="file"
@@ -69,15 +54,7 @@ const SourceUploadModal = ({
             onDrop={handleDrop}
             className={`flex h-full cursor-pointer flex-col items-center justify-center gap-3 rounded-2xl border border-dashed border-neutral-600 py-6 ${dragActive ? "bg-neutral-700" : "bg-neutral-900"}`}
           >
-            <h2 className="h-14 w-14 rounded-full bg-neutral-600 p-3.5 text-2xl font-black leading-6 text-neutral-400">
-              ⇪
-            </h2>
-            <h3 className="text-xl font-bold text-neutral-300">소스 업로드</h3>
-            <p className="leading-loose text-neutral-500">
-              업로드할 파일을 선택하거나 드래그 앤 드롭하세요.
-              <br />
-              지원되는 파일 형식: PDF
-            </p>
+            <ModalContents />
           </label>
         </div>
       </div>
