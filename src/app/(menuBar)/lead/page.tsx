c5c4ea2adf6cf7be209a3a3f95d1626a96959c16
kimsohyeon <aotoyae@gmail.com>
2024-12-16 17:54:28 +0900

    refactor: 리드 헤더 컴포넌트 분리

--- src/app/(menuBar)/lead/page.tsx
+++ src/app/(menuBar)/lead/page.tsx
@@ -1,5 +1,6 @@
 import Details from "@/components/domain/lead/details/Details";
 import FollowUp from "@/components/domain/lead/follow-up/FollowUp";
+import LeadHeader from "@/components/domain/lead/LeadHeader";
 import Prompt from "@/components/domain/lead/propmt/Prompt";
 import Summary from "@/components/domain/lead/summary/Summary";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
@@ -7,27 +8,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 const page = () => {
   return (
     <main className="w-full bg-white">
-      <section className="flex w-full flex-col items-center justify-center">
-        <h2 className="py-4 font-bold">그린몰치과 인공지능 아바타 판매</h2>
-        <ul className="grid h-10 w-full grid-cols-8 border-y border-solid border-neutral-400">
-          <li className="flex items-center justify-center border-r border-solid border-neutral-400 bg-neutral-200">
-            리드구분
-          </li>
-          <li className="flex items-center justify-center">기업</li>
-          <li className="flex items-center justify-center border-x border-solid border-neutral-400 bg-neutral-200">
-            리드명
-          </li>
-          <li className="flex items-center justify-center">LG / 김경헌 팀장</li>
-          <li className="flex items-center justify-center border-x border-solid border-neutral-400 bg-neutral-200">
-            담당 세르파
-          </li>
-          <li className="flex items-center justify-center">홍길동</li>
-          <li className="flex items-center justify-center border-x border-solid border-neutral-400 bg-neutral-200">
-            생성일
-          </li>
-          <li className="flex items-center justify-center">2024-10-10</li>
-        </ul>
-      </section>
+      <LeadHeader />
       <Tabs defaultValue="prompt" className="w-full">
         <TabsList className="grid w-full grid-cols-4">
           {/* 리드 아이템 목록 */}
