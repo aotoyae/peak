4c0db09bce1a556b44ec140ddf5594801869cd6a
kimsohyeon <aotoyae@gmail.com>
2024-12-16 16:13:42 +0900

    fix: peak 타이틀 로고 이미지로 변경

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,5 @@
+import PEAK_LOGO from "@/assets/peak_logo_wt.png";
+import Image from "next/image";
 import Link from "next/link";
 
 const page = () => {
@@ -5,7 +7,7 @@ const page = () => {
     <main className="grid h-screen grid-cols-2">
       <section className="flex flex-col justify-between bg-black p-6 text-white">
         <Link href="/">
-          <h1 className="text-4xl font-bold">peak</h1>
+          <Image src={PEAK_LOGO} alt="peak logo" width={100} />
         </Link>
         <div className="flex flex-col gap-3">
           <p>&ldquo;이곳에서 세상의 의미 있는 세일즈가 이루어지길.&rdquo;</p>
