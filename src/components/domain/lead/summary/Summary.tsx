a7d54f4ca8e71bb58339a069e7a821a65dc0151b
kimsohyeon <aotoyae@gmail.com>
2024-12-16 17:50:09 +0900

    refactor: HI 분석 컴포넌트 분리

--- src/components/domain/lead/summary/Summary.tsx
+++ src/components/domain/lead/summary/Summary.tsx
@@ -1,4 +1,5 @@
 import TabHeader from "../TabHeader";
+import HIAnalysis from "./HIAnalysis";
 
 const Summary = () => {
   return (
@@ -11,13 +12,7 @@ const Summary = () => {
         <p className="h-full w-full rounded border border-solid border-neutral-300 bg-neutral-100 p-2"></p>
         <button className="rounded bg-neutral-300 px-10 py-2">저장하기</button>
       </section>
-      <form className="flex h-[580px] flex-col items-end justify-center gap-4 p-4">
-        <textarea
-          className="h-full w-full rounded border border-solid border-neutral-300 bg-neutral-100 p-2"
-          placeholder="메모 작성"
-        ></textarea>
-        <button className="rounded bg-neutral-300 px-10 py-2">저장하기</button>
-      </form>
+      <HIAnalysis />
     </main>
   );
 };
