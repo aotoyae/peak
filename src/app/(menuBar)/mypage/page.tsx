506933a3344a0e210249d5f9debae66cf0a5f1e7
kimsohyeon <aotoyae@gmail.com>
2024-12-16 17:15:54 +0900

    refactor: 파트너 정보 컴포넌트 분리

--- src/app/(menuBar)/mypage/page.tsx
+++ src/app/(menuBar)/mypage/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import AnalysisForm from "@/components/domain/mypage/AnalysisForm";
+import PartnerInfo from "@/components/domain/mypage/PartnerInfo";
 import SarpaInfo from "@/components/domain/mypage/SarpaInfo";
 
 const page = () => {
@@ -10,21 +11,7 @@ const page = () => {
         MY PAGE
       </h1>
       <div className="flex w-full flex-col items-center justify-center gap-8">
-        <section className="w-full border-b border-solid border-neutral-300 p-8">
-          <h2 className="pb-4 font-bold">기본 정보</h2>
-          <ul className="grid h-10 grid-cols-4 border border-solid border-neutral-400">
-            <li className="flex items-center justify-center border-r border-solid border-neutral-400 bg-neutral-200">
-              회사명
-            </li>
-            <li className="flex items-center justify-center">
-              선한인공지능연구소
-            </li>
-            <li className="flex items-center justify-center border-x border-solid border-neutral-400 bg-neutral-200">
-              대표
-            </li>
-            <li className="flex items-center justify-center">홍길동</li>
-          </ul>
-        </section>
+        <PartnerInfo />
         <AnalysisForm />
         <SarpaInfo />
       </div>
