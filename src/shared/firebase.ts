6fe08a0f1d8b8fe328f1284244d58f5d6b7c49bb
kimsohyeon <aotoyae@gmail.com>
2024-12-23 19:26:11 +0900

    delete: analytice 제거

--- src/shared/firebase.ts
+++ src/shared/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/compat/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore/lite";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,5 +19,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const db = getFirestore(app);
